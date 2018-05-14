42c7471d7b32d46f180bba18a213b3cc81cd27f7
Emily Schleiner <cordial.emily@gmail.com>
2018-05-14 09:37:29 -0700

    footer.php added

---
+++ footer.php
@@ -0,0 +1,17 @@
+<?php
+/* Footer template */
+ ?>
+
+<footer id="footer">
+  <div class="row">
+    <div class="col-lg-12">
+      <h4 class="text-center f-blog-name">&copy; <?php echo date("Y"); echo " "; bloginfo('name'); ?></li></h4>
+    </div>
+  </div>
+
+</footer>
+
+
+  <?php wp_footer(); ?>
+</body>
+</html>

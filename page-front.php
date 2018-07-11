ed7223f3e222d74197816351b7fcde4850f77e48
Emily Schleiner <cordial.emily@gmail.com>
2018-07-11 13:53:19 -0700

    events-calendar-shortcode to front page to list events coming up in calendar

--- page-front.php
+++ page-front.php
@@ -14,6 +14,7 @@
 <!-- Middle content section -->
 <section class="row mb-2">
         <div class="col-md-6">
+          <strong class="d-inline-block mb-2 text-primary">Posts</strong>
           <?php
           $postslist = get_posts('orderby=menu_order&sort_order=asc');
           foreach ($postslist as $post) :
@@ -22,7 +23,7 @@
           <div class="card flex-md-row mb-4 box-shadow h-md-250">
 
             <div class="card-body d-flex flex-column align-items-start">
-              <strong class="d-inline-block mb-2 text-primary">Post</strong>
+
               <h3 class="mb-0"><a class="text-dark" href="<?php the_permalink(); ?>"><?php echo mb_strimwidth( get_the_title(), 0, 30, '...' );  ?></a></h3>
 
                   <div class="entry">
@@ -30,26 +31,15 @@
                       <p class="card-text mb-auto"><?php the_excerpt(); ?></p>
                   </div>
             </div>
-
             <?php the_post_thumbnail( 'dms-custom-img', array( 'class' => 'card-img-right')); ?>
-            <!--<img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Thumbnail [200x250]" style="width: 200px; height: 250px;" src="" data-holder-rendered="true">-->
-
           </div>
           <?php endforeach; ?>
         </div>
+
         <div class="col-md-6">
-          <div class="card flex-md-row mb-4 box-shadow h-md-250">
-            <div class="card-body d-flex flex-column align-items-start">
-              <strong class="d-inline-block mb-2 text-success">Design</strong>
-              <h3 class="mb-0">
-                <a class="text-dark" href="#">Post title</a>
-              </h3>
-              <div class="mb-1 text-muted">Nov 11</div>
-              <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-              <a href="#">Continue reading</a>
-            </div>
-            <img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Thumbnail [200x250]" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22250%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20250%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_163601fe5ac%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A13pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_163601fe5ac%22%3E%3Crect%20width%3D%22200%22%20height%3D%22250%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2256.1953125%22%20y%3D%22130.75625%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true" style="width: 200px; height: 250px;">
-          </div>
+          <strong class="d-inline-block mb-2 text-success">Events</strong>
+          <p class="card-text mb-auto"><?php echo do_shortcode("[ecs-list-events excerpt='true' eventdetails='true' ecs-list-events thumb='true' thumbwidth='200' thumbheight='250' contentorder='title, date, excerpt, venue, thumbnail']"); ?></p>
+
         </div>
 </section>
 

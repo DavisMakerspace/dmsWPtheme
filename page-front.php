b91c650d8576ccfbe3fe4e005de6106dad0abdfc
Emily Schleiner <cordial.emily@gmail.com>
2018-07-20 15:30:40 -0700

    page-front now has a container-fluid wrapped around the Post and Event section

--- page-front.php
+++ page-front.php
@@ -12,8 +12,11 @@
 </section>
 
 <!-- Middle content section -->
-<section class="row mb-2">
+<section class="container-fluid">
+  <div class="row mb-2">
         <div class="col-md-6">
+
+          <!-- Posts -->
           <strong class="d-inline-block mb-2 text-primary">Posts</strong>
           <?php
           $postslist = get_posts('orderby=menu_order&sort_order=asc');
@@ -21,11 +24,8 @@
               setup_postdata($post);
               ?>
           <div class="card flex-md-row mb-4 box-shadow h-md-250">
-
             <div class="card-body d-flex flex-column align-items-start">
-
               <h3 class="mb-0"><a class="text-dark" href="<?php the_permalink(); ?>"><?php echo mb_strimwidth( get_the_title(), 0, 30, '...' );  ?></a></h3>
-
                   <div class="entry">
                       <div class="mb-1 text-muted"><?php the_time('F jS, Y'); ?></div>
                       <p class="card-text mb-auto"><?php the_excerpt(); ?></p>
@@ -36,11 +36,12 @@
           <?php endforeach; ?>
         </div>
 
+        <!-- Events -->
         <div class="col-md-6">
           <strong class="d-inline-block mb-2 text-success">Events</strong>
           <p class="card-text mb-auto"><?php echo do_shortcode("[ecs-list-events excerpt='true' eventdetails='true' ecs-list-events thumb='true' thumbwidth='200' thumbheight='250' contentorder='title, date, excerpt, venue, thumbnail']"); ?></p>
-
         </div>
+  </div>
 </section>
 
 

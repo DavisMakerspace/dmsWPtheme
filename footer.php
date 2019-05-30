05b48b52c12d1b21435ed01ddd35842e1fa0bb5f
Emily Schleiner <cordial.emily@gmail.com>
2019-05-30 09:49:29 -0700

    sticky footer

--- footer.php
+++ footer.php
@@ -3,9 +3,11 @@
  ?>
 
 <footer class="" id="footer">
-  <nav class="navbar navbar-light dms-terracotta">
-    <p class="f-blog-name mx-auto mt-3 mb-3 text-uppercase"><strong>&copy; <?php echo date("Y"); echo " "; bloginfo('name'); ?></strong></p>
-  </nav>
+
+    <nav class="navbar navbar-light dms-terracotta">
+      <p class="f-blog-name mx-auto mt-3 mb-3 text-uppercase"><strong>&copy; <?php echo date("Y"); echo " "; bloginfo('name'); ?></strong></p>
+    </nav>
+  
 </footer>
 
   <?php wp_footer(); ?>
